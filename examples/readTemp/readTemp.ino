6892bef31271ed20bf808553f0698ad443781c5c
Frances9 <44355133+Frances9@users.noreply.github.com>
2019-08-23 16:15:04 +0800

    Update readTemp.ino

--- examples/readTemp/readTemp.ino
+++ examples/readTemp/readTemp.ino
@@ -80,56 +80,58 @@ void setup(void) {
     eValue3=4, if four successive temperatures are more than threshold value, OS output active state; 
     eValue4=6, if six successive temperatures are more than threshold value, OS output active state.
    */
-  Serial.print("OS故障队列: ");
+  Serial.print("OS fault queue: ");
   Serial.print(lm75b.getQueueValue());
-  Serial.println(" (OS输出变为active状态需要连续满足环境温度大于阈值温度的次数)");
-  //用户设定值，环境温度超出此值时引起OS状态改变
-  /*getTosC函数的作用时获取Tos寄存器里面存储的阈值大小，
+  Serial.println(" (The number of times that ambient temperature consecutively exceeds threshold to make OS output become active state.)");
+  //User-defined value, OS state changes when ambient temperature exceeds this value.
+  /*getTosC Get the threshold value stored in Tos register. 
   */
-  Serial.print("阈值温度(摄氏度): ");
+  Serial.print("Threshold (degree Celsius): ");
   /**
-   * @brief 获取阈值温度(Tos:Overtemperature shutdown).
-   * @return 返回温度值，单位是摄氏度.
-   * @n 温度值范围是 -55°C 到 +125°C.
+   * @brief Get threshold temperature(Tos:Overtemperature shutdown).
+   * @return Return temperature value, unit: °C
+   * @n Detection range: -55°C to +125°C.
    */
   Serial.print(lm75b.getTosC());
   Serial.println("°C");
   /**
-   * @brief 获取阈值温度(Tos:Overtemperature shutdown).
-   * @return 返回温度值，单位是华氏度.
-   * @n 温度范围是 -67°F 到 +257°F.
+   * @brief Get threshold temperature(Tos:Overtemperature shutdown).
+   * @return Return temperature value, unit: °F 
+   * @n Detection range: -67°F to +257°F.
    */
-  //Serial.print("阈值温度(华氏度): ");
+  //Serial.print("Threshold (Fahrenheit): ");
   //Serial.print(lm75b.getTosF());
   //Serial.println("°F");
   /*!
-    getHysteresisC函数的作用时获取Thyst寄存器里面存储的滞后限制温度的大小，
-    用户设定的滞后温度，会让OS电平的跳变从环境温度小于阈值温度时跳变滞后到小于滞后限制温度时跳变.
-    滞后限制温度产生的效果：当温度大于阈值温度时，OS Pin 变为活跃状态(默认为低电平)，当温度小于阈
-    值温度时，OS Pin状态不会立即恢复正常状态(默认为高电平)，而是会延迟到小于滞后温度时才会恢复正常状态 
+    getHysteresisC Get hysteresis temperature stored in Thyst register
+    User-defined hysteresis temperature; delay the level jump of OS: OS level will jump when the
+    ambient temperature is less than hysteresis value instead of threshold value.
+    Effect: when the temperature is more than threshold temperature, OS pin becomes active(default LOW)
+    When the temperature is less than threshold temperature, OS pin will not back to the normal 
+    state(default HIGH) until the temperature value is less than the hysteresis tempreature.
   */
-  Serial.print("滞后温度(华氏度): ");
+  Serial.print("Hysteresis temperature (degree Celsius): ");
   Serial.print(lm75b.getHysteresisC());
   Serial.println("°C");
   /**
-   * @brief 获取滞后限制温度(自定义的温度点，小等于于阈值温度)..
-   * @return 返回温度值，单位是华氏度.
-   * @n 温度范围是 -67°F 到 +257°F.
+   * @brief Get hysteresis temperature (User-defined temperature, ≤ Threshold)..
+   * @return Return temperature, unit: °F 
+   * @n Detection range: -67°F to +257°F.
    */
-  //Serial.print("滞后温度(华氏度): ");
+  //Serial.print("Hysteresis temperature (Fahrenheit): ");
   //Serial.print(lm75b.getHysteresisF());
   //Serial.println("°F");
   Serial.println("**-----------------------------------------------------**");
 }
 
 void loop(void) {
-  Serial.print("环境温度(摄氏度): ");
-  Serial.print(/*摄氏温度=*/lm75b.getTemperatureC());
+  Serial.print("Ambient temperature (°C): ");
+  Serial.print(/*degree Celsius=*/lm75b.getTemperatureC());
   Serial.println("°C");
-  Serial.print("环境温度(华氏度): ");
-  Serial.print(/*华氏温度=*/lm75b.getTemperatureF());
+  Serial.print("Ambient temperature (°F): ");
+  Serial.print(/*Fahrenheit=*/lm75b.getTemperatureF());
   Serial.println("°F");
   Serial.println("");
-  // 设定的延时应大于100ms；
+  // delay should be over 100ms;
   delay(1000);
 }
