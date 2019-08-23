d92ebede1bf7e03fbeeb6b1c7f5988f0dafcd33f
Frances9 <44355133+Frances9@users.noreply.github.com>
2019-08-23 17:32:53 +0800

    Update thermostat.ino

--- examples/thermostat/thermostat.ino
+++ examples/thermostat/thermostat.ino
@@ -37,118 +37,121 @@
 //Use this constructor function to define software IIC and change chip address. 
 //DFRobot_LM75B lm75b(&Wire, 0x48);
 DFRobot_LM75B lm75b; 
-//OS引脚连接Arduino数字引脚2，通过引脚2监控OS脚的电平变化
+//Connect OS pin to Arduino digital pin 2, monitor the level of OS pin via pin 2. 
 #define OSPin   (2)
-//state是OS 引脚的电平 1为正常状态，0为活跃状态
+//State: the LEVEL of OS pin, 1 normal, 0 active
 volatile  int state = 1;
-//false 表示还没有进行恒温调节
-//true  表示已经开始进行恒温调节
+//false thermostatic regulation not start 
+//true  thermostatic regulation starts
 volatile bool thermostatState = false;
 /*!
-  thermostat 中断函数，功能是提示恒温器升温或降温.
+  thermostat Interrupt function, prompt thermostat to increase or decrease temperature.
 */
 void thermostat(){
-  //因为 polarity 选择的是active LOW模式，所以当温度值大于阈值温度，OS输出为低电平
-  //将恒温点定在阈值温度处，温度变化的下限不能低于滞后限制温度  
+  //Since polarity is set to active LOW mode, OS output LOW when temperature is over threshold
+  //Define the constant temperature point at threshold point, the minimum temperature should be greater than hysteresis value.
   state = 1 - state;
-  //开始进行恒温调节
+  //Start thermostatic regulation 
   thermostatState = true;
 }
 void setup(void) {
   Serial.begin(115200);
-  //检测IIC是否能正常通信
+  //Detect of IIC communication works properly
   while (lm75b.begin() != 0) {
-    Serial.println("IIC初始化失败，请检测连线是否正确");
+    Serial.println("IIC init failed, please check if the connection is correct?");
     delay(1000);
   }
-  attachInterrupt(/*中断号0代表2号数字脚*/0,thermostat, CHANGE);  //设置触发类型为CHANGE，中断号0，即数字2口
+  attachInterrupt(/*Interrupt number 0 represents digital pin2*/0,thermostat, CHANGE);  //Set trigger type to CHANGE, interrupt number 0(D Pin2)
   pinMode(OSPin, INPUT);
   /**
-   @brief 自定义阈值温度(Tos:Overtemperature shutdown)
-   @param 温度值，单位是摄氏度，需满足Tos%0.5 == 0 ；
-   @n 范围是 -55°C 到 +125°C
+   @brief Define threshold temperature(Tos:Overtemperature shutdown)
+   @param Temperature, unit:°C, Tos%0.5 == 0 ；
+   @n Detection range: -55°C  to +125°C
   */
   lm75b.setTosC(/*Tos=*/33);
-  //使用华氏度对阈值寄存器设置    
+  //Set threshold register to Fahrenheit    
   //lm75b.setTosF(/*Tos=*/91);
   /**
-   @brief 自定义滞后限制温度
-   @param 温度值，单位是摄氏度，需满足Thyst%0.5 == 0 ；
-   @n 范围是 -55°C 到 +125°C,Thyst 必须小于等于 Tos 的值.
-   @n 因为滞后限制值是阈值温度的下偏差的限制值，所以设定的温度应应尽量接近阈值温度，以达到比较精确地控制温度.
-   @n 用户设定的滞后温度，会让OS电平的跳变从环境温度小于阈值温度时跳变滞后到小于滞后限制温度时再跳变.
-   @n 滞后限制温度产生的效果：当温度大于阈值温度时，OS Pin 变为活跃状态(默认为低电平)，当温度小于阈
-   @n 值温度时，OS Pin状态不会立即恢复正常状态(默认为高电平)，而是会延迟到小于滞后温度时才会恢复正常状态 
+   @brief Defien hysteresis temperature 
+   @param  Temperature, unit: °C, Thyst%0.5 == 0;
+   @n Detection range: -55°C to +125°C, Thyst must be smaller than Tos;
+   @n The pre-set temeprature should approach threshold temeprature so as to control temperature accurately. 
+   @n User-defined hysteresis temperature; delay the level jump of OS: OS level will jump when the
+   @n ambient temperature is less than hysteresis value instead of threshold value.
+   @n Effect: when the temperature is more than threshold temperature, OS pin becomes active(default LOW)
+   @n         When the temperature is less than threshold temperature, OS pin will not back to the normal
+   @n state(default HIGH) until the temperature value is less than the hysteresis tempreature. 
   */
   lm75b.setHysteresisC(/*Thyst=*/32);
-  //使用华氏度对滞后寄存器设置
+  //Set hysteresis register to Fahrenheit  
   //lm75b.setHysteresisF(/*Thyst=*/91);
   /*!
-    只有满足故障队列数，OS才会产生中断
-    value的取值为：
-    温度寄存器存储的温度值在每次转换完成之后，会自动与阈值温度和滞后温度相比较。
-    eValue1，需满足一次温度值大于阈值温度。若满足则OS输出为active状态；
-    eValue2，需满足连续二次温度值大于阈值温度。若满足则OS输出为active状态。
-    eValue3，需满足连续四次次温度值大于阈值温度。若满足则OS输出为active状态。
-    eValue4，需满足连续六次温度值大于阈值温度。若满足则OS输出为active状态。
+    OS generates interrupt only when there is enough number of queue faults. 
+    value: 
+    Each time the temperature in the temperature register completes conversion, it will automatically
+    be compared with threshold and hystersis temperature. 
+    eValue1, if temperature is more than threshold value once, OS output active state; 
+    eValue2, if two successive temperatures are more than threshold value, OS output active state; 
+    eValue3, if four successive temperatures are more than threshold value, OS output active state; 
+    eValue4, if six successive temperatures are more than threshold value, OS output active state.
   */
   lm75b.setQueueValue(/*value=*/lm75b.eValue4);
   
   Serial.println("**-----------------------------------------------------**");
-  //用户设定值，环境温度超出此值时引起OS状态变为active
-  /*getTosC函数的作用时获取Tos寄存器里面存储的阈值大小，
+  //User-defined value, OS state changes to active when ambient temperature exceeds this value.
+  /*getTosCGet the threshold value stored in Tos register.
   */
-  Serial.print("阈值温度(摄氏度): ");
+  Serial.print("Threshold (degree Celsius): ");
   Serial.print(lm75b.getTosC());
   Serial.println("°C");
   /**
-   * @brief 获取阈值温度(Tos:Overtemperature shutdown).
-   * @return 返回温度值，单位是华氏度.
-   * @n 温度范围是 -67°F 到 +257°F.
+   * @brief Get threshold temperature(Tos:Overtemperature shutdown).
+   * @return Return temperature value, unit: °F 
+   * @n Detection range: -67°F to +257°F.
    */
-  //Serial.print("阈值温度(华氏度): ");
+  //Serial.print("Threshold (Fahrenheit): ");
   //Serial.print(lm75b.getTosF());
   //Serial.println("°F");
   /*!
-    getHysteresisC函数的作用时获取Thyst寄存器里面存储的滞后限制温度的大小，
+    getHysteresisC Get hysteresis temperature stored in Thyst register
   */
-  Serial.print("滞后温度(摄氏度): ");
+  Serial.print("Hysteresis temperature (degree Celsius): ");
   Serial.print(lm75b.getHysteresisC());
   Serial.println("°C");
   /**
-   * @brief 获取滞后限制温度(自定义的温度点，小等于于阈值温度)..
-   * @return 返回温度值，单位是华氏度.
-   * @n 温度范围是 -67°F 到 +257°F.
+   * @brief Get hysteresis temperature (User-defined temperature, ≤ Threshold)..
+   * @return Return temperature, unit: °F 
+   * @n Detection range: -67°F to +257°F.
    */
-  //Serial.print("阈值温度(华氏度): ");
+  //Serial.print("Threshold temperature (Fahrenheit): ");
   //Serial.print(lm75b.getTosF());
   //Serial.println("°F");
   Serial.println("**-----------------------------------------------------**");
     if(lm75b.getTemperatureC()<lm75b.getHysteresisC()) {
-      Serial.println("环境温度低于滞后温度，请升温");
+      Serial.println("Ambient temperature less than Hysteresis, increase the temperature");
     }
     else if(lm75b.getTemperatureC()>lm75b.getTosC()){
-      Serial.println("环境温度高于阈值温度，请降温");
+      Serial.println("Ambient temperature more than Threshold, decrease the temperature");
     }
     else{
-      Serial.println("温度正常");
+      Serial.println("Temperature is fine");
     }
 }
 
 void loop(void) {
   /*!
-   默认设置 device operation mode selection：(0*)normal
+   Default setting: device operation mode selection：(0*)normal
             OS operation mode selection    ：(0*)OS comparator
             OS polarity selection          ：(0*)OS active LOW
             OS fault queue programming     ：(00*)queue value = 1
             reserved                       ： 000*
   */
-  //因为 polarity 选择的是active LOW模式，所以当温度值大于阈值温度，OS输出为低电平
-  //将恒温点定在阈值温度处，温度变化的下限不能低于滞后限制温度
+  //Since polarity is set to active LOW mode, OS output LOW when temperature is over threshold
+  //Define the constant temperature point at threshold point, the minimum temperature should be greater than hysteresis value.
   delay(2000);
-  Serial.print("环境温度: ");
-  /*getTempC 获取环境温度*/
-  Serial.print(/*温度=*/lm75b.getTemperatureC());
+  Serial.print("Ambient Temperature: ");
+  /*getTempC Get ambient temperature*/
+  Serial.print(/*Temperature=*/lm75b.getTemperatureC());
   Serial.println("°C");
   if((state == 0) && (thermostatState == true)){
    Serial.println("降温阶段，将温度降至滞后温度以下");
